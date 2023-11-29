92273c6d1f2591c74e657224ae01bc695d9ca60b
lotsofninjas <PelleB.86@gmail.com>
2023-11-29 08:16:29 +0100

    slutver

--- Labb 2/Program.cs
+++ Labb 2/Program.cs
@@ -39,6 +39,7 @@ while (menuLoop)
     switch (menuChoice)
     {
         case 1:
+            Console.Clear();
             Console.WriteLine("Hur många medlemmar finns i familjen?");
             try
             {
@@ -48,30 +49,38 @@ while (menuLoop)
             {
                 Console.WriteLine("Använd siffror");
             }
-
+            Console.Clear();
             listName = new string[familyMembers];
             listAge = new int[familyMembers];
+            Console.Clear();
 
             for (int i = 0; i < familyMembers; i++)
             {
                 Console.Write($"Ange namn på familjemedlem {i + 1}: ");
                 name = Console.ReadLine();
+                Console.Clear();
 
                 Console.Write($"{name}s ålder är: ");
                 if (!int.TryParse(Console.ReadLine(), out age))
                 {
+                    Console.Clear();
                     Console.WriteLine("Siffror min vän, siffror. På det igen!");
                     i--;
+                    Console.ReadLine();
+                    Console.Clear();
                     continue;
                 }
                 listName[i] = name;
                 listAge[i] = age;
                 //So I don't have to do a whole new loop for age in case 3 and 4
                 totalAge += age;
+                Console.Clear();
             }
+            Console.Clear();
             break;
 
         case 2:
+            Console.Clear();
             if (familyMembers == 0)
             {
                 Console.WriteLine("Du måste lägga till familjemedlemmarna först");
@@ -84,33 +93,46 @@ while (menuLoop)
                     Console.WriteLine($"{listName[i]} som är {listAge[i]} år");
                 }
             }
+            Console.ReadLine();
+            Console.Clear();
             break;
 
         case 3:
+            Console.Clear();
             if (familyMembers == 0)
             {
+                Console.Clear();
                 Console.WriteLine("Du måste lägga till familjemedlemmarna först");
             }
             else
             {
                 Console.WriteLine($"Familjens totala ålder är {totalAge} år");
+
             }
+            Console.ReadLine();
+            Console.Clear();
             break;
 
         case 4:
+            Console.Clear();
             if (familyMembers == 0)
             {
+                Console.Clear();
                 Console.WriteLine("Du måste lägga till familjemedlemmarna först");
+                Console.ReadLine();
             }
             else
             {
                 averageAge = Math.Round(totalAge / familyMembers, 2);
                 Console.WriteLine($"Familjens medelålder är: {averageAge:F2}");
+                Console.ReadLine();
             }
+            Console.Clear();
             break;
 
         case 5:
-            Console.WriteLine("Tack för mig!");
+            Console.Clear();
+            Console.WriteLine("Tack för mig");
             Console.ReadLine();
             menuLoop = false;
             break;
